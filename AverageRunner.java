c8b6b8bceab9518e1e09e9f8885b430dee9fba49
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-10-07 11:23:17 -0500

    Add files via upload

---
+++ AverageRunner.java
@@ -0,0 +1,15 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class AverageRunner
+{
+	public static void main( String[] args )
+   {
+ 		//add more test cases
+	}
+}
